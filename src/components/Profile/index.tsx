82179d0e00cb2a807c86c77ac3235a0190797bb2
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-12 00:53:23 +0900

    ✨ UserIconコンポーネントを作成

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,7 +1,10 @@
+import { UserIcon } from "./UserIcon"
+
 export const Profile = () => {
   return (
     <>
       <div className="flex-center flex-col w-full h-full">
+        <UserIcon />
       </div>
     </>
   )
