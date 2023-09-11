d01a312f35d899512352840873565ad7178498a9
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-12 01:07:29 +0900

    ✨ UserNameコンポーネントを作成

---
+++ src/components/Profile/UserName/index.tsx
@@ -0,0 +1,10 @@
+export const UserName = () => {
+  return (
+    <>
+      <div className="text-center">
+        <h2 className="text-4xl font-bold">テツカ ヒロキ</h2>
+        <p>tzp</p>
+      </div>
+    </>
+  )
+}
--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,12 @@
 import { UserIcon } from "./UserIcon"
+import { UserName } from "./UserName"
 
 export const Profile = () => {
   return (
     <>
-      <div className="flex-center flex-col w-full h-full">
+      <div className="flex-center flex-col gap-2 w-full h-full">
         <UserIcon />
+        <UserName />
       </div>
     </>
   )
