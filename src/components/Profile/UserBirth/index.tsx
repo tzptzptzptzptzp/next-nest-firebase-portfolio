7a27373d073991914be9c27c832ef75df770755b
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-13 01:21:00 +0900

    💄 UserBirthのスタイルを調整

--- src/components/Profile/UserBirth/index.tsx
+++ src/components/Profile/UserBirth/index.tsx
@@ -1,11 +1,11 @@
-import { faCakeCandles } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faCakeCandles } from "@fortawesome/free-solid-svg-icons"
 
 export const UserBirth = () => {
   return (
     <>
-      <div className="flex-center gap-2">
-        <FontAwesomeIcon icon={faCakeCandles} width={'1em'} />
+      <div className="flex-center gap-1">
+        <FontAwesomeIcon icon={faCakeCandles} height={'1em'} />
         <p>1994.5</p>
       </div>
     </>
