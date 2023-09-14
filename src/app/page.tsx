3ccfffbe331464a5c1cab8b5dd24584de3e15755
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-15 01:40:36 +0900

    💄 全体のカラーレイアウトを変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <main className="flex-center relative centering-x w-screen h-screen">
       <div className="flex relative w-full h-full">
-        <div className='relative top-0 left-0 w-1/3 h-full'>
-          <Profile />
-        </div>
-        <div className='relative top-0 right-0 overflow-y-scroll w-2/3 h-full'>
-
+        <Profile />
+        <div className='relative top-0 right-0 w-2/3 h-full py-24 pr-24 bg-main dark:bg-bg_dark'>
+          <div className='overflow-y-scroll h-full'>
+            <div className='bg-bg_light dark:bg-bg_dark'></div>
+          </div>
         </div>
       </div>
     </main>
--- src/components/Profile/UserIcon/index.tsx
+++ src/components/Profile/UserIcon/index.tsx
@@ -3,7 +3,7 @@ export const UserIcon = () => {
   return (
     <div>
       <Image
-        className='rounded-full border-4 border-amber-200 bg-amber-200'
+        className='rounded-full border-[6px] border-bg_light dark:border-text_dark bg-bg_light dark:bg-text_dark'
         src="/images/bisyojo_chan_500.png"
         width={250}
         height={250}
