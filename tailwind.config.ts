bc141106b17279762d09c5acbcc10a24ab716c43
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-15 00:54:17 +0900

    🔧 TailwindCSS設定ファイルを更新

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,28 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      s: { max: "767px" },
+      m: { min: "768px", max: "1023px" },
+      l: { min: "1024px", max: "1399px" },
+      ll: { min: "1400px" },
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        main: "#FDE68A",
+        bg_light: "#FFF",
+        bg_dark: "#222",
+        text_light: "#222",
+        text_dark: "#EEE",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
