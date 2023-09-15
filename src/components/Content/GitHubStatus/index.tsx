631a6ddce4235d09fab93b7b99221358e56f19cd
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-16 01:48:53 +0900

    ✨ GitHubStatusコンポーネントを作成

---
+++ src/components/Content/GitHubStatus/index.tsx
@@ -0,0 +1,13 @@
+export const GitHubStatus = () => {
+  const username = 'tzptzptzptzptzp'
+  return (
+    <>
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between gap-8">
+        </div>
+        <div className="w-full">
+        </div>
+      </div>
+    </>
+  )
+}
