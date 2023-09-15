c3acba1abed435a7bfb709b1d1fa36c94d87e123
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-16 01:38:12 +0900

    ✨ コンテンツをコンポーネントに分離

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Content } from '@/components/Content'
 import { Profile } from '@/components/Profile'
 
 export default function Home() {
@@ -5,11 +6,7 @@ export default function Home() {
     <main className="flex-center relative centering-x w-screen h-screen">
       <div className="flex relative w-full h-full">
         <Profile />
-        <div className='relative top-0 right-0 w-2/3 h-full py-24 pr-24 bg-main dark:bg-bg_dark'>
-          <div className='overflow-y-scroll h-full'>
-            <div className='bg-bg_light dark:bg-bg_dark'></div>
-          </div>
-        </div>
+        <Content />
       </div>
     </main>
   )
---
+++ src/components/Content/index.tsx
@@ -0,0 +1,12 @@
+export const Content = () => {
+  return (
+    <>
+      <div className='relative top-0 right-0 w-2/3 h-full py-24 pr-24 bg-main dark:bg-bg_dark'>
+        <div className='overflow-y-scroll h-full'>
+          <div className='p-16 bg-bg_light dark:bg-bg_dark'>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
