0a4c83f771d3c269222c32673d81908c369c12e6
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-16 23:43:34 +0900

    💄 全体のレイアウトを調整

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -6,7 +6,7 @@ export const Content = () => {
     <>
       <div className='relative top-0 right-0 w-2/3 h-full py-24 pr-24 bg-main dark:bg-bg_dark'>
         <div className='overflow-y-scroll h-full'>
-          <div className='p-16 bg-bg_light dark:bg-bg_dark'>
+          <div className='flex flex-col gap-16 p-16 bg-bg_light dark:bg-bg_dark'>
             <GitHubStatus />
             <Favorite />
           </div>
