10c2db502b7a0d8aafc7251bd99682f0d577d4d3
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-17 01:45:16 +0900

    🐛 文字がボーダーに被る不具合を修正

--- src/components/Content/Title/index.tsx
+++ src/components/Content/Title/index.tsx
@@ -4,7 +4,7 @@ import { IconDefinition } from "@fortawesome/fontawesome-common-types"
 export const ContentTitle = ({ icon, title }: { icon: IconDefinition, title: string }) => {
   return (
     <>
-      <div className="flex items-center gap-2 pb-1 border-b text-5xl" >
+      <div className="flex items-center gap-2 pb-3 border-b text-5xl" >
         <div style={{ transform: 'translateY(1px)' }}>
           <FontAwesomeIcon icon={icon} height={'.9em'} className="max-w-[.9em] max-h-[.9em]" />
         </div>
