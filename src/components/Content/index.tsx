1eecba8b65d895c5f0ffd99ee27256eff73a6215
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-18 23:35:58 +0900

    ✨ Experienceコンポーネントを作成

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -2,6 +2,7 @@ import { GitHubStatus } from "./GitHubStatus"
 import { Favorite } from "./Favorite"
 import { Enhancing } from "./Enhancing"
 import { Career } from "./Career"
+import { Experience } from "./Experience"
 
 export const Content = () => {
   return (
@@ -9,6 +10,7 @@ export const Content = () => {
       <div className='relative top-0 right-0 w-2/3 h-full py-24 pr-24 bg-main dark:bg-bg_dark'>
         <div className='overflow-y-scroll h-full'>
           <div className='flex flex-col gap-16 p-16 bg-bg_light dark:bg-bg_dark'>
+            <Experience />
             <GitHubStatus />
             <Favorite />
             <Enhancing />
