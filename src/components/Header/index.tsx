db008b99f97f8e21b5911f0f1e2b9cf59879db20
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-23 23:23:29 +0900

    ✨ Navigationコンポーネントを作成

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
+import { Navigation } from "../Navigation"
+
 export const Header = () => {
   return (
     <>
       <header className="fixed right-0 centering-y z-20 w-48">
+        <Navigation />
       </header>
     </>
   )
