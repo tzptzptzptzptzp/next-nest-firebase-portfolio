e845aed3fb5e3d32f2cb7f11dc7a3836c14c30fe
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-25 01:47:24 +0900

    ✨ テーマを切り替える機能を実装

--- src/components/Navigation/ThemeSwitch.tsx
+++ src/components/Navigation/ThemeSwitch.tsx
@@ -1,10 +1,34 @@
+"use client"
+
+import { SunIcon } from "../Icon/Sun"
 import { MoonIcon } from "../Icon/Moon"
 
+import { useAppDispatch, useAppSelector } from "@/redux/hooks"
+import { toggleTheme } from "@/redux/themeSlice"
+
 export const ThemeSwitch = () => {
+  const isTheme = useAppSelector((state) => state.theme.value)
+  const dispatch = useAppDispatch()
+
+  const handleClick = (currentTheme: string) => {
+    if (currentTheme === 'light') {
+      dispatch(toggleTheme('dark'))
+    } else if (currentTheme === 'dark') {
+      dispatch(toggleTheme('light'))
+    }
+  }
   return (
     <>
-      <button className="w-[4.5rem] h-[4.5rem]">
-        <MoonIcon />
+      <button onClick={() => handleClick(isTheme)} className="w-[4.5rem] h-[4.5rem]">
+        {isTheme === '' ? (
+          <MoonIcon />
+        ) : null}
+        {isTheme === 'light' ? (
+          <MoonIcon />
+        ) : null}
+        {isTheme === 'dark' ? (
+          <SunIcon />
+        ) : null}
       </button>
     </>
   )
