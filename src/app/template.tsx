4a0470805ef3b1b21950bb60f7d6287ff708a7ac
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-24 23:36:37 +0900

    ✨ マウント時にテーマ設定を取得し状態を管理する処理を実装

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,6 +1,11 @@
 "use client"
 
+import { useEffect } from 'react'
 import { Zen_Maru_Gothic } from 'next/font/google'
+
+import { useDispatch, useSelector } from 'react-redux'
+import { toggleTheme } from '@/redux/themeSlice'
+import { RootState } from '@/redux/store'
 
 const fontMain = Zen_Maru_Gothic({ weight: ['500', '700'], subsets: ['latin'] })
 
@@ -9,6 +14,32 @@ export default function Template({
 }: {
   children: React.ReactNode
 }) {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    const setInitialColorMode = () => {
+      const getInitialColorMode = () => {
+        const storedPreferenceMode = localStorage.getItem('theme')
+        const hasStoredPreference = typeof storedPreferenceMode === 'string'
+        if (hasStoredPreference) {
+          return storedPreferenceMode
+        }
+
+        const preference = matchMedia('(prefers-color-scheme): dark')
+        const hasMediaQueryPreference = typeof preference.matches === 'boolean'
+        if (hasMediaQueryPreference) {
+          return preference.matches ? 'dark' : 'light'
+        }
+
+        return 'light'
+      }
+      const currentColorMode = getInitialColorMode()
+      dispatch(toggleTheme(
+        currentColorMode
+      ))
+    }
+    setInitialColorMode()
+  }, [])
   return (
     <>
       <html lang="ja">
