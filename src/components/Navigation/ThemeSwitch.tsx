a6840616a45948281e32b0e96e776203b3441f4c
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-25 01:46:12 +0900

    ✨ ThemeSwitchコンポーネントを作成

---
+++ src/components/Navigation/ThemeSwitch.tsx
@@ -0,0 +1,11 @@
+import { MoonIcon } from "../Icon/Moon"
+
+export const ThemeSwitch = () => {
+  return (
+    <>
+      <button className="w-[4.5rem] h-[4.5rem]">
+        <MoonIcon />
+      </button>
+    </>
+  )
+}
