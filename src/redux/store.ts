faa669f2b00a3194780862e2993483711bf18b29
tzptzptzptzptzp <tzp2539@gmail.com>
2023-09-27 01:09:03 +0900

    🔧 StoreにlangReducerを追加

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import themeReducer from "./themeSlice";
+import langReducer from "./langSlice";
 
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
+    lang: langReducer,
   },
 });
 
