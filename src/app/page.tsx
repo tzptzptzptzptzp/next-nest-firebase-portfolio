9c689fc29b64351d912e6f3ec92fa1b5818d8344
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-01 23:39:05 +0900

    ✨データを取得

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import { Content } from '@/components/Content'
 import { Header } from '@/components/Header'
 import { Profile } from '@/components/Profile'
 
+import { useAppSelector } from "@/redux/hooks"
+
+import { dataType } from "@/types/data.type"
+
 export default function Home() {
+  const data: dataType = useAppSelector((state) => state.data.data)
+
   return (
     <main className="flex-center relative centering-x w-screen h-screen">
       <div className="flex relative w-full h-full">
