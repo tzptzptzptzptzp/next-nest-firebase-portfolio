8c902bf58a582c12e65bc6f252dd1971c9169577
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-02 01:02:17 +0900

    ⚡️ 言語設定に応じて職業名を切り替える処理を追加

--- src/components/Profile/UserJob/index.tsx
+++ src/components/Profile/UserJob/index.tsx
@@ -1,10 +1,13 @@
+import { useAppSelector } from "@/redux/hooks"
+
 import { occupationType } from "@/types/data.type"
 
 export const UserJob = ({ data }: { data: occupationType }) => {
+  const currentLang = useAppSelector((state) => state.lang.value)
   return (
     <>
       <div className="text-center">
-        <p className="text-[1.8rem]">WebDeveloper<br />Designer / Creator</p>
+        <p className="text-[1.8rem]" dangerouslySetInnerHTML={{ __html: currentLang === 'en' ? data.en : data.ja }}></p>
       </div>
     </>
   )
