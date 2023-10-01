69f3bb755c8ab0bfe37a34908a7de5208881cc2e
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-02 00:58:56 +0900

    ⚡️ 言語設定に応じて名前を切り替える処理を追加

--- src/components/Profile/UserName/index.tsx
+++ src/components/Profile/UserName/index.tsx
@@ -1,11 +1,14 @@
+import { useAppSelector } from "@/redux/hooks"
+
 import { nameType } from "@/types/data.type"
 
 export const UserName = ({ data }: { data: nameType }) => {
+  const currentLang = useAppSelector((state) => state.lang.value)
   return (
     <>
       <div className="text-center">
-        <h2 className="text-4xl font-bold">テツカ ヒロキ</h2>
-        <p>tzp</p>
+        <h2 className="text-4xl font-bold">{currentLang === 'en' ? data.name.en : data.name.ja}</h2>
+        <p>{data.nickname}</p>
       </div>
     </>
   )
