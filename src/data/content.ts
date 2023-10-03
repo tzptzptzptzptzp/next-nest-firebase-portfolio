99d6c60b0166a7db9e28ddf76035d750b243daab
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-04 00:13:33 +0900

    🔧 コンテンツ切り替え用のファイルを作成

---
+++ src/data/content.ts
@@ -0,0 +1,23 @@
+export const HomeContent = {
+  aboutMe: true,
+  gitHubStatus: true,
+  favorite: true,
+  enhancing: true,
+  skill: false,
+  project: false,
+  portfolio: false,
+  career: false,
+  experience: true,
+};
+
+export const SkillContent = {
+  aboutMe: false,
+  gitHubStatus: false,
+  favorite: false,
+  enhancing: false,
+  skill: true,
+  project: true,
+  portfolio: true,
+  career: true,
+  experience: false,
+};
