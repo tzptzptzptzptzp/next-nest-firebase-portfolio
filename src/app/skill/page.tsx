0aef0ed7131c50e0d2a6a5558c98a906eb793fa1
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-04 00:17:05 +0900

    ✨ スキルページを作成

---
+++ src/app/skill/page.tsx
@@ -0,0 +1,25 @@
+"use client"
+
+import { Content } from '@/components/Content'
+import { Header } from '@/components/Header'
+import { Profile } from '@/components/Profile'
+
+import { useAppSelector } from "@/redux/hooks"
+
+import { SkillContent as content } from '@/data/content'
+
+import { dataType } from "@/types/data.type"
+
+export default function Skill() {
+  const data: dataType = useAppSelector((state) => state.data.data)
+
+  return (
+    <main className="flex-center relative centering-x w-screen h-screen">
+      <div className="flex relative w-full h-full">
+        <Header />
+        <Profile data={data.profile} />
+        <Content data={data.content} content={content} />
+      </div>
+    </main>
+  )
+}
