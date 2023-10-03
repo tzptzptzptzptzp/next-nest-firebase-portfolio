a61739674f37bcf595336a909d6a1fd17257a43a
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-04 00:16:42 +0900

    ⚡️ ページ毎にコンテンツを切り替える処理を追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,17 +6,18 @@ import { Profile } from '@/components/Profile'
 
 import { useAppSelector } from "@/redux/hooks"
 
+import { HomeContent as content } from '@/data/content'
+
 import { dataType } from "@/types/data.type"
 
 export default function Home() {
   const data: dataType = useAppSelector((state) => state.data.data)
-
   return (
     <main className="flex-center relative centering-x w-screen h-screen">
       <div className="flex relative w-full h-full">
         <Header />
         <Profile data={data.profile} />
-        <Content data={data.content} />
+        <Content data={data.content} content={content} />
       </div>
     </main>
   )
