183ec99438ea95d0a33f6a54ce0b57a4a788e716
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-14 00:07:10 +0900

    ✨ 共通テキストコンポーネントを作成

---
+++ src/frontend/src/components/elements/Text/index.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from "react"
+
+import type { TextType } from "../elements.type"
+
+export const Text = forwardRef<HTMLParagraphElement, TextType>(
+  ({
+    bold = false, children, color = 'text-current', size = '1.6rem', style = ''
+  }, ref) => (
+    <>
+      <p
+        className={`${bold && 'font-bold'} ${size} ${color} ${style}`}
+        ref={ref}
+      >
+        {children}
+      </p>
+    </>
+  )
+)
