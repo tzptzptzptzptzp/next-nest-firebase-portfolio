848898df2549689be7a51fd82da481cdf5c9d19f
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-18 00:44:01 +0900

    ✨ サインアップに必要なフォーム項目を作成

--- src/frontend/src/app/signup/page.tsx
+++ src/frontend/src/app/signup/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
-import { TextInput } from '@/components/ui/TextInput'
 import { ChangeEvent, useCallback, useState } from 'react'
+import { FormTextInput } from '@/components/ui/Form/FormTextInput'
+
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+import { UserSchema } from '@/schema/user'
 
 export default function SignUp() {
   const [userName, setUserName] = useState('')
@@ -9,24 +14,65 @@ export default function SignUp() {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
+  const defaultValues = UserSchema.defaultValues
+
+  const { control, handleSubmit, reset } = useForm({
+    defaultValues,
+    mode: 'onChange',
+    resolver: zodResolver(UserSchema.signUpSchema)
+  })
+
   const inputUserName = useCallback((e: ChangeEvent<HTMLInputElement>) => {
     setUserName(e.target.value)
   }, [setUserName])
 
+  const inputEmail = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value)
+  }, [setEmail])
+
+  const inputPassword = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }, [setPassword])
+
+  const inputConfirmPassword = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setConfirmPassword(e.target.value)
+  }, [setConfirmPassword])
+
   return (
     <>
       <main className="flex-center relative centering-x w-screen h-screen">
-        <div className="flex relative w-1/3 h-auto">
-          <TextInput
-            fullWidth={true}
+        <div className="flex flex-col gap-8 relative w-1/3 h-auto p-16 rounded-3xl color-bg">
+          <FormTextInput
+            control={control}
+            isRequired
             label={'ユーザー名'}
-            variant='standard'
-            multiline={false}
-            required={true}
-            rows={1}
-            value={userName}
-            type={'text'}
+            maxLength={20}
+            name={'userName'}
             onChange={inputUserName}
+          />
+          <FormTextInput
+            control={control}
+            isRequired
+            label={'メールアドレス'}
+            maxLength={100}
+            name={'email'}
+            onChange={inputEmail}
+          />
+          <FormTextInput
+            control={control}
+            isRequired
+            label={'パスワード'}
+            maxLength={100}
+            name={'password'}
+            onChange={inputPassword}
+          />
+          <FormTextInput
+            control={control}
+            isRequired
+            label={'パスワード(確認)'}
+            maxLength={100}
+            name={'confirmPassword'}
+            onChange={inputConfirmPassword}
           />
         </div>
       </main>
