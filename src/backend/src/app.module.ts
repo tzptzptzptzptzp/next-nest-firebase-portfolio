ddc247dbd67bd9992c10fc8781212430a72ffda7
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-27 23:10:39 +0900

    🔧 firebase.module/serviceを生成

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [],
+  imports: [FirebaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
