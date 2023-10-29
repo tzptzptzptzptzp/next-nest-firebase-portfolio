d5b5fbaf48cd6418bbae2c016165711861c95a46
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-29 23:43:32 +0900

    🚧 WIP:サインアップ時に実行される関数を実装中

--- src/backend/src/users/users.controller.ts
+++ src/backend/src/users/users.controller.ts
@@ -1,4 +1,22 @@
 import { Controller } from "@nestjs/common";
+import { FirebaseService } from "src/firebase/firebase.service";
+
+import { UsersEntity } from "./model/users.entity";
 
 @Controller("users")
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly firebaseService: FirebaseService) {}
+
+  /**
+   * サインアップ時に実行されるユーザー作成関数
+   */
+  async createUserData(uid: string, email: string): Promise<string> {
+    const userRef = await this.firebaseService.getCollectionRef("users");
+    const accountData = new UsersEntity({
+      userName: "",
+      userImage: "",
+      role: "guest",
+    });
+    return "";
+  }
+}
