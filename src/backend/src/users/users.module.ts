d0c2a1ec8199077bdaed39e3280dbf27612babdf
tzptzptzptzptzp <tzp2539@gmail.com>
2023-10-30 23:14:07 +0900

    🔧 imports, exportsを記述

--- src/backend/src/users/users.module.ts
+++ src/backend/src/users/users.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { Module } from "@nestjs/common";
+import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
+import { FirebaseModule } from "src/firebase/firebase.module";
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  imports: [FirebaseModule],
+  providers: [UsersService],
 })
 export class UsersModule {}
