785f4f2a8538d75c0e40cc3db6f40b2fb9302f8f
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-02 00:10:42 +0900

    ⚡️ CORS設定を追加

--- src/backend/src/main.ts
+++ src/backend/src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const allowedOrigins: Array<string> = ["http://localhost:3000"];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+  });
+
   await app.listen(8080);
 }
 bootstrap();
