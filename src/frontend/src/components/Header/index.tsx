b977939b270990fa6442816a5efe85641c043a4c
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-09 21:57:31 +0900

    🔥 不要なフラグメントを削除

--- src/frontend/src/components/Header/index.tsx
+++ src/frontend/src/components/Header/index.tsx
@@ -2,10 +2,8 @@ import { Navigation } from "../Navigation"
 
 export const Header = () => {
   return (
-    <>
-      <header className="fixed right-0 centering-y z-20 w-48">
-        <Navigation />
-      </header>
-    </>
+    <header className="fixed right-0 centering-y z-20 w-48">
+      <Navigation />
+    </header>
   )
 }
