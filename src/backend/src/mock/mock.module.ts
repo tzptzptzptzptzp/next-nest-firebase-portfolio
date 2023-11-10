ad2e4ba039880cb222d96a91c8e631ebe2a50c0c
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-10 23:08:28 +0900

    ✨ mock.serviceを生成

--- src/backend/src/mock/mock.module.ts
+++ src/backend/src/mock/mock.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MockController } from './mock.controller';
+import { MockService } from './mock.service';
 
 @Module({
-  controllers: [MockController]
+  controllers: [MockController],
+  providers: [MockService]
 })
 export class MockModule {}
