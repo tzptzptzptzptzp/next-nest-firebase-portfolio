9146262c6fd52b88e4ea42614060c1bb4131a87d
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-11 23:50:21 +0900

    ✨ APIルーティングを定義

--- src/backend/src/mock/mock.controller.ts
+++ src/backend/src/mock/mock.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from "@nestjs/common";
+import { MockService } from "./mock.service";
 
-@Controller('mock')
-export class MockController {}
+@Controller("mock")
+export class MockController {
+  constructor(private mockService: MockService) {}
+
+  @Get()
+  getMockData() {
+    return this.mockService.getMockData();
+  }
+}
