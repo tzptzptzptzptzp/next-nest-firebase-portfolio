3d784571e8b20c23b75e67bd39b2b6dcb2530d5f
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-12 23:30:06 +0900

    🔧 返却するモックデータを作成

---
+++ src/backend/src/mock/data/mockData.ts
@@ -0,0 +1,235 @@
+export const mockData = {
+  profile: {
+    name: {
+      en: "Hiroki Tetsuka",
+      ja: "テツカ ヒロキ",
+    },
+    nickname: "tzp",
+    occupation: {
+      en: "WebDeveloper<br>Designer / Creator",
+      ja: "エンジニア<br>デザイナー / クリエイター",
+    },
+    date: {
+      birth: "1994.5",
+      engineering: "2022.2",
+    },
+    sns: {
+      github: "tzptzptzptzptzp",
+      x: "tzptzptzptzptzp",
+      instagram: "",
+    },
+    link: ["https://design-tomato.com/"],
+  },
+  content: {
+    aboutMeData: {
+      en: "Working at an IT venture company in Tokyo, I am primarily involved in contract development.\nWhile I initially focused on frontend development, I am now also engaged in backend development.\nIn my personal time, I actively keep up with various technologies.\nMy future goal is to become a high - level full - stack engineer.\nI am dedicated to contributing to my GitHub repositories to demonstrate my commitment.",
+      ja: "都内のITベンチャー企業で受託開発をしています。\n元はフロントエンドを主軸に開発を行っていましたが、現在はバックエンドの開発も行っています。\nプライベートでは積極的に色々な技術をキャッチアップしています。\n将来はハイレベルなフルスタックエンジニアを目指しています。GitHubの草生やし活動に勤しんでいます。",
+    },
+    favoriteData: [
+      "aws",
+      "gcp",
+      "azure",
+      "react",
+      "vue",
+      "flutter",
+      "graphql",
+      "gulp",
+      "redux",
+      "supabase",
+      "vercel",
+    ],
+    enhancingData: [
+      "aws",
+      "gcp",
+      "azure",
+      "react",
+      "vue",
+      "flutter",
+      "graphql",
+      "gulp",
+      "redux",
+      "supabase",
+      "vercel",
+    ],
+    skillData: [
+      {
+        sectionTitle: "Language & Framework",
+        skillSet: [
+          {
+            icon: "react",
+            name: "React",
+            rate: 4,
+          },
+          {
+            icon: "nextjs",
+            name: "Next.js",
+            rate: 4,
+          },
+          {
+            icon: "ts",
+            name: "TypeScript",
+            rate: 4,
+          },
+          {
+            icon: "rails",
+            name: "Ruby on Rails",
+            rate: 3,
+          },
+        ],
+      },
+      {
+        sectionTitle: "DataBase",
+        skillSet: [
+          {
+            icon: "mysql",
+            name: "MySQL",
+            rate: 4,
+          },
+          {
+            icon: "postgresql",
+            name: "PostgreSQL",
+            rate: 4,
+          },
+          {
+            icon: "mongodb",
+            name: "mongoDB",
+            rate: 4,
+          },
+          {
+            icon: "",
+            name: "Hoge",
+            rate: 2,
+          },
+          {
+            icon: "",
+            name: "Hoge",
+            rate: 2,
+          },
+        ],
+      },
+    ],
+    projectData: [
+      {
+        title: {
+          en: "Financial Media Feature Enhancement",
+          ja: "金融系メディア機能改修",
+        },
+        description: {
+          en: "Responsible for implementing new features in a Laravel application",
+          ja: "Laravelアプリケーションの新規機能実装などを担当",
+        },
+        technology: ["Laravel", "WordPress", "EC2"],
+        completion_date: "2023-10",
+      },
+      {
+        title: {
+          en: "Sustainable Web Application",
+          ja: "サスティナブルWebアプリケーション",
+        },
+        description: {
+          en: "Responsible for the renewal of an existing application, including frontend and backend implementation",
+          ja: "既存アプリのリニューアル フロントエンド、バックエンドの実装を担当",
+        },
+        technology: ["Next.js", "Redux", "Nest.js", "Firebase"],
+        completion_date: "2023-11",
+      },
+      {
+        title: {
+          en: "Web Customer Interaction Tool Application",
+          ja: "Web接客ツールアプリケーション",
+        },
+        description: {
+          en: "Responsible for implementing the frontend of a SaaS product for Inside Sales",
+          ja: "インサイドセールス向けSaaS フロントエンドの実装を担当",
+        },
+        technology: ["React", "Next.js", "Redux"],
+        completion_date: "2023-12",
+      },
+    ],
+    portfolioData: [
+      {
+        name: {
+          en: "Tetsuka Hiroki Profile Site",
+          ja: "テツカ ヒロキ プロフィールサイト",
+        },
+        description: {
+          en: "This is the profile site you are currently viewing. Data can be modified through the admin panel, and data is saved in Firebase via Nest.js.",
+          ja: "今ご覧になっているプロフィールサイトです。管理画面からデータの変更が可能で、Nest.jsを経由しFirebaseにデータを保存しています。",
+        },
+        link: "",
+        technology: ["Next.js", "Nest.js", "Firebase"],
+      },
+      {
+        name: {
+          en: "Web no Arekore",
+          ja: "WEBのあれこれ",
+        },
+        description: {
+          en: "Sharing JavaScript recipes that can be used with copy and paste (compatible with TypeScript) and expertise that makes website development easier.",
+          ja: "コピペで使えるJavaScriptレシピ(TypeScript対応)やよりサイト制作を行いやすくなるノウハウを発信中",
+        },
+        link: "https://xn--l8j8a4kb.website/",
+        technology: ["Next.js", "microCMS", "recoil"],
+      },
+      {
+        name: {
+          en: "Design Tomato",
+          ja: "Design Tomato",
+        },
+        description: {
+          en: "A web development company operated by me. It has been designed with a strong focus on design and code architecture.",
+          ja: "自身が運営するWeb制作会社です。デザインとコードの設計に力を入れて制作しました。",
+        },
+        link: "https://design-tomato.com/",
+        technology: ["WordPress", "PHP", "MAMP"],
+      },
+    ],
+    careerData: [
+      {
+        name: {
+          en: "Infrastructure company sales position",
+          ja: "インフラ系営業職",
+        },
+        description: {
+          en: "I was formerly employed in a non-IT company in a sales role. While working there, I began self-studying programming.",
+          ja: "非IT企業の営業職です。在職中にプログラミングの勉強を独学で始めました。",
+        },
+        join: "2019.9",
+        leave: "2021.5",
+        link: "",
+      },
+      {
+        name: {
+          en: "Design Tomato",
+          ja: "Design Tomato",
+        },
+        description: {
+          en: "After leaving the company, I started working as a freelancer, specializing in web design and development.",
+          ja: "会社を退職し、フリーランスとしてWeb制作やデザインをスタートしました。",
+        },
+        join: "2022.2",
+        leave: "",
+        link: "https://design-tomato.com/",
+      },
+      {
+        name: {
+          en: "WEEL.inc",
+          ja: "WEEL.inc",
+        },
+        description: {
+          en: "I am now associated with an IT venture company, where I have transitioned from web design to application development. I enjoy my work every day.",
+          ja: "ITベンチャー企業です。Web制作からアプリケーション開発へステップアップしました。毎日楽しくお仕事しています。",
+        },
+        join: "2023.7",
+        leave: "",
+        link: "https://weel.co.jp/company/",
+      },
+    ],
+    experienceData: {
+      technology:
+        "babel,bash,bootstrap,css,deno,emotion,express,firebase,git,gulp,html,js,jquery,laravel,linux,md,materialui,mongodb,mysql,nestjs,nextjs,nodejs,php,postgres,prisma,pug,rails,react,redux,regex,ruby,sass,sqlite,styledcomponents,supabase,svelte,tailwind,threejs,ts,vercel,vim,vite,vue,webpack,wordpress,windicss",
+      application:
+        "ae,atom,autocad,blender,codepen,discord,figma,github,ai,instagram,linkedin,ps,postman,powershell,pr,twitter,unity,vscode,xd",
+    },
+  },
+};
