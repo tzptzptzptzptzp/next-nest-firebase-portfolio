c1691e60da031f175653894b3f17bbc0c0674b35
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-21 23:20:59 +0900

    ✨ 読み込み中はスケルトンスクリーンを表示するように変更

--- src/frontend/src/components/Content/Career/index.tsx
+++ src/frontend/src/components/Content/Career/index.tsx
@@ -1,19 +1,27 @@
 import { ContentTitle } from "../Title"
 import { CareerBox } from "./Box"
+import { Skeleton } from "@/components/molecules/Skeleton"
 import { faRoute } from "@fortawesome/free-solid-svg-icons"
 
 import { careerType } from "@/types/data.type"
 
-export const Career = ({ data }: { data: careerType }) => {
+type Props = {
+  data: careerType
+  loading?: boolean
+}
+
+export const Career = ({ data, loading = false }: Props) => {
   return (
     <div>
       <div className="flex flex-col gap-10">
         <ContentTitle icon={faRoute} title={'Career'} />
-        <div className="flex flex-col gap-6">
-          {data.map((item, i) => (
-            <CareerBox key={i} data={item} i={i} length={data.length} />
-          ))}
-        </div>
+        <Skeleton isLoading={loading} width={'100%'} height={100}>
+          <div className="flex flex-col gap-6">
+            {data.map((item, i) => (
+              <CareerBox key={i} data={item} i={i} length={data.length} />
+            ))}
+          </div>
+        </Skeleton>
       </div>
     </div>
   )
