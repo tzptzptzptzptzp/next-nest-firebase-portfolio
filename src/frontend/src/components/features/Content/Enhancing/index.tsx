e622728d46615576e45b25768b5719fbc9f606d5
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-24 23:27:36 +0900

    ✨ 読み込み中はスケルトンスクリーンを表示するように変更

--- src/frontend/src/components/features/Content/Enhancing/index.tsx
+++ src/frontend/src/components/features/Content/Enhancing/index.tsx
@@ -2,18 +2,22 @@ import { ContentTitle } from "../Title"
 import { faRocket } from "@fortawesome/free-solid-svg-icons"
 
 import { enhancingType } from "@/types/data.type"
+import { Skeleton } from "@/components/molecules/Skeleton"
 
 type Props = {
   data: enhancingType
+  loading?: boolean
 }
 
-export const Enhancing = ({ data }: Props) => {
+export const Enhancing = ({ data, loading = false }: Props) => {
   return (
     <div>
       <div className="flex flex-col gap-10">
         <ContentTitle icon={faRocket} title={'Enhancing'} />
-        {/* eslint-disable-next-line */}
-        <img src={`https://skillicons.dev/icons?i=${data}`} alt="" />
+        <Skeleton isLoading={loading} width={'100%'} height={100}>
+          {/* eslint-disable-next-line */}
+          <img src={`https://skillicons.dev/icons?i=${data}`} alt="" />
+        </Skeleton>
       </div>
     </div>
   )
--- src/frontend/src/components/features/Content/index.tsx
+++ src/frontend/src/components/features/Content/index.tsx
@@ -28,7 +28,7 @@ export const Content = ({ data, content }: { data: contentType, content: pageCon
         <div className='flex flex-col gap-16 p-16 color-bg'>
           {content.gitHubStatus && (<GitHubStatus />)}
           {content.favorite && (<Favorite data={data.favoriteData} loading={loading} />)}
-          {content.enhancing && (<Enhancing data={data.enhancingData} />)}
+          {content.enhancing && (<Enhancing data={data.enhancingData} loading={loading} />)}
           {content.aboutMe && (<AboutMe data={data.aboutMeData} loading={loading} />)}
           {content.skill && (<Skill data={data.skillData} loading={loading} />)}
           {content.project && (<Project data={data.projectData} loading={loading} />)}
