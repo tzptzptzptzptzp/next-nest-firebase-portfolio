e6e80bfb7ad61ba714f8d58a2012dd687384c8a0
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-24 23:25:41 +0900

    ⚡️ データに基づいたEnhancing画像を生成するように変更

--- src/frontend/src/components/features/Content/Enhancing/index.tsx
+++ src/frontend/src/components/features/Content/Enhancing/index.tsx
@@ -3,13 +3,17 @@ import { faRocket } from "@fortawesome/free-solid-svg-icons"
 
 import { enhancingType } from "@/types/data.type"
 
-export const Enhancing = ({ data }: { data: enhancingType }) => {
+type Props = {
+  data: enhancingType
+}
+
+export const Enhancing = ({ data }: Props) => {
   return (
     <div>
       <div className="flex flex-col gap-10">
         <ContentTitle icon={faRocket} title={'Enhancing'} />
         {/* eslint-disable-next-line */}
-        <img src="https://skillicons.dev/icons?i=aws,gcp,azure,react,vue,flutter,graphql,gulp,nextjs,php,redux,supabase,vercel" alt="" />
+        <img src={`https://skillicons.dev/icons?i=${data}`} alt="" />
       </div>
     </div>
   )
