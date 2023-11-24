24c1a525e299ba09f609adae2e55ee4325e9ebe6
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-24 23:20:15 +0900

    ⚡️ データに基づいたFavorite画像を生成するように変更

--- src/frontend/src/components/features/Content/Favorite/index.tsx
+++ src/frontend/src/components/features/Content/Favorite/index.tsx
@@ -3,13 +3,17 @@ import { faStar } from "@fortawesome/free-regular-svg-icons"
 
 import { favoriteType } from "@/types/data.type"
 
-export const Favorite = (data: { data: favoriteType }) => {
+type Props = {
+  data: favoriteType
+}
+
+export const Favorite = ({ data }: Props) => {
   return (
     <div>
       <div className="flex flex-col gap-10">
         <ContentTitle icon={faStar} title={'Favorite'} />
         {/* eslint-disable-next-line */}
-        <img src="https://skillicons.dev/icons?i=aws,gcp,azure,react,vue,flutter,graphql,gulp,nextjs,php,redux,supabase,vercel" alt="" />
+        <img src={`https://skillicons.dev/icons?i=${data}`} alt="Favorite Skills" />
       </div>
     </div>
   )
