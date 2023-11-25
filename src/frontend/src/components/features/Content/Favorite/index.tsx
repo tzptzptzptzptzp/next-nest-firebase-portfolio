5808033f345df277ee00fc9762b9b1eb8af77632
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-26 02:43:29 +0900

    ⚡️ Favorite画像の読み込みを最適化

--- src/frontend/src/components/features/Content/Favorite/index.tsx
+++ src/frontend/src/components/features/Content/Favorite/index.tsx
@@ -1,23 +1,21 @@
 import { ContentTitle } from "../Title"
-import { Skeleton } from "@/components/molecules/Skeleton"
+import { Image } from "@/components/atoms/Image"
 import { faStar } from "@fortawesome/free-regular-svg-icons"
 
 import { favoriteType } from "@/types/data.type"
 
 type Props = {
   data: favoriteType
-  loading?: boolean
 }
 
-export const Favorite = ({ data, loading = false }: Props) => {
+export const Favorite = ({ data }: Props) => {
   return (
     <div>
       <div className="flex flex-col gap-10">
         <ContentTitle icon={faStar} title={'Favorite'} />
-        <Skeleton isLoading={loading} width={'100%'} height={100}>
-          {/* eslint-disable-next-line */}
-          <img src={`https://skillicons.dev/icons?i=${data}`} alt="Favorite Skills" />
-        </Skeleton>
+        <div className="flex flex-col w-full">
+          <Image alt="Favorite Skills" className="w-full h-auto" src={`https://skillicons.dev/icons?i=${data}`} unoptimized />
+        </div>
       </div>
     </div>
   )
