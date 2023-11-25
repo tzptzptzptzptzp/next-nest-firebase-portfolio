d185e71271eb80c3e2068cc970afe0667ddc09a7
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-26 02:43:45 +0900

    💄 余白を調整

--- src/frontend/src/components/features/Content/GitHubStatus/index.tsx
+++ src/frontend/src/components/features/Content/GitHubStatus/index.tsx
@@ -18,7 +18,7 @@ export const GitHubStatus = () => {
   return (
     <div className="flex flex-col relative z-0">
       <ContentTitle icon={faGithubAlt} title={'GitHub Status'} />
-      <div className="flex justify-between gap-8 -z-10">
+      <div className="flex justify-between gap-8 -z-10 min-h-[2.5rem]">
         <div className="w-2/5">
           <Image alt="Top Languages Card" className={`${styles.scaleUp} w-full h-auto`} skeleton={false} src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${userName}&layout=compact&show_icons=true&hide_title=true&hide_border=true&hide=scss,css,html${isTheme === 'dark' ? '&theme=' + user.theme + '&bg_color=' + user.bg_color : ''}`} unoptimized />
         </div>
@@ -26,7 +26,7 @@ export const GitHubStatus = () => {
           <Image alt="GitHub Stats Card" className={`${styles.scaleUp} w-full h-auto`} skeleton={false} src={`https://github-readme-stats.vercel.app/api?username=${userName}&show_icons=true&count_private=true&hide_title=true&hide_border=true&hide=contribs${isTheme === 'dark' ? '&theme=' + user.theme + '&bg_color=' + user.bg_color : ''}`} unoptimized />
         </div>
       </div>
-      <div className="w-full">
+      <div className="flex flex-col w-full">
         <Image alt="GitHub Profile Trophy" className="w-full h-auto" src={`https://github-profile-trophy.vercel.app/?username=${userName}&row=1&column=7&no-frame=true&no-bg=true${isTheme === 'dark' ? '&theme=' + user.theme : ''}`} unoptimized />
       </div>
     </div>
