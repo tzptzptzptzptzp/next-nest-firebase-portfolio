fb20e304d20227d83fd7274bafc3b66d0c908486
tzptzptzptzptzp <tzp2539@gmail.com>
2023-11-27 22:44:07 +0900

    💄 ローディング時のレイアウト調整

--- src/frontend/src/components/features/Content/AboutMe/index.tsx
+++ src/frontend/src/components/features/Content/AboutMe/index.tsx
@@ -16,9 +16,9 @@ export const AboutMe = ({ data, loading = false }: Props) => {
   return (
     <div className="flex flex-col gap-10">
       <ContentTitle icon={faAddressCard} title={'About me'} />
-      <div className="w-full px-4">
+      <div className="w-full">
         <Skeleton isLoading={loading} width={'100%'} height={100}>
-          <p className="w-full text-[1.8rem] leading-relaxed whitespace-pre-wrap" dangerouslySetInnerHTML={{
+          <p className="w-full px-4 text-[1.8rem] leading-relaxed whitespace-pre-wrap" dangerouslySetInnerHTML={{
             __html: currentLang === 'en' ? data.en : data.ja
           }}>
           </p>
